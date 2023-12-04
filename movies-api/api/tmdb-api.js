9be86655c1b8e74827e562edf8757b9cf077e2bd
Evan Hearne <evanhearne02@gmail.com>
2023-12-04 16:58:03 +0000

    Added week 11 exercise (genres)

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 export const getUpcomingMovies = async () => {
     try {
         const response = await fetch(
-            `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}`
         );
 
         if (!response.ok) {
@@ -14,4 +14,20 @@ export const getUpcomingMovies = async () => {
     } catch (error) {
         throw error;
     }
+};
+
+export const getGenres = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en`
+        );
+
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+
+	   return await response.json();
+	} catch (error) {
+	    throw error;
+	}
 };
--- movies-api/index.js
+++ movies-api/index.js
@@ -16,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/movies',authenticate,  moviesRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
